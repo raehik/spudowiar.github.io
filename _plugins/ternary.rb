87ab67fc4ff0ba99ccfa599b560eb7eba25b9e2a
Saleem Rashid <spudowiar@outlook.com>
2015-08-25 16:01:28 +0100

    jekyll: slight changes

--- _plugins/ternary.rb
+++ _plugins/ternary.rb
@@ -1,28 +1,26 @@
 # syntactic sugar filter (Jekyll::Ternary) {{{
-module Jekyll
-    module Ternary
-        # Ternary operator {{ value | if: truth | else: untruth }} {{{
-        def if(value, truth)
-            value ? truth : nil
-        end
-
-        def else(value, untruth)
-            value ? value : untruth
-        end
-        # }}}
-        # Fallback {{ first | fallback: second, third, etc. }} {{{
-        def fallback(value, *fallbacks)
-            while fallbacks.length > 0
-                if value.to_s.empty?
-                    value = fallbacks.shift
-                else
-                    break
-                end
-            end
-            value
-        end
-        # }}}
+module Jekyll::Ternary
+    # Ternary operator {{ value | if: truth | else: untruth }} {{{
+    def if(value, truth)
+        value ? truth : nil
     end
+
+    def else(value, untruth)
+        value ? value : untruth
+    end
+    # }}}
+    # Fallback {{ first | fallback: second, third, etc. }} {{{
+    def fallback(value, *fallbacks)
+        while fallbacks.length > 0
+            if value.to_s.empty?
+                value = fallbacks.shift
+            else
+                break
+            end
+        end
+        value
+    end
+    # }}}
 end
 # }}}
 
